82e5f727009aa3a0845f0a72fbaa04537a5e7042
Hesam Ro <hesamroustaei80@gmail.com>
2025-06-27 22:42:35 +0330

    Add decrypt function and add menu for user

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# morse code translator
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
@@ -15,32 +14,58 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
                     '(':'-.--.', ')':'-.--.-',' ':'/'}
 
+REVERSE_MORSE_CODE_DICT = {v:k for k,v in MORSE_CODE_DICT.items()}
 
-def encryption(text):
-    #Translate the plain text to morse code
-    encypted_text = ""
-    #Check if user wrote any text
-    if text:
-        text = text.upper()
-        for letter in text:
-            if letter in MORSE_CODE_DICT:
-                encypted_text += MORSE_CODE_DICT[letter]
-            else:
-                encypted_text += "?"
-
-        return encypted_text
-    else:
-        print("At least write 1 character!")
-        main()
+def encrypt(text):
+    encrypted_text = ""
+    text = text.upper()
+    for letter in text:
+        encrypted_text += MORSE_CODE_DICT.get(letter,'?') + " "
+    return encrypted_text.strip()
+       
+def decrypt(morse_code):
+    decoded_text  = ""
+    words = morse_code.split(" / ")
+    for word in words:
+        #Chop the word in to seperate letters to translate letter by letter
+        letters = word.split()
+        for letter_code in letters:
+            decoded_text += REVERSE_MORSE_CODE_DICT.get(letter_code, "?")
+        decoded_text += " "
+    return decoded_text.strip()
 
 def main():
     while True:
-        user_input = input("Enter your text: ")
-        if not user_input:
-            print("Enter something atleast!")
-            continue
-        morse_output = encryption(user_input)
-        print(morse_output)
+        try:
+            choice = input("\n(D) decrypt, (E) encrypt, (Q) quit: ").strip().upper()
+
+            if choice == "E":
+                user_text = input("Enter your text: ").strip()
+                if not user_text:
+                    print("Please enter something!")
+                    continue
+                morse_output = encrypt(user_text)
+                print("Morse Code:", morse_output)
+                continue
+
+            elif choice == "D":
+                user_morse_code = input("Enter your Morse Code: ").strip()
+                if not user_morse_code:
+                    print("Please enter something!")
+                    continue
+                text_output = decrypt(user_morse_code)
+                print("Decoded :", text_output)
+
+            elif choice == "Q":
+                print("Bye!")
+                break
+
+            else:
+                print("Invalid choice. Please select E, D, or Q.")
+
+        except Exception as e:
+            print("Oops! Something went wrong:", e)
+            print("Let's try again.\n")
 
 if __name__ == "__main__":
     main()
